f25e71f4cb4b2d81a166b6a5e95b9a8a442fcb34
travislai0706 <58297822+travislai0706@users.noreply.github.com>
2020-02-02 22:44:35 +0800

    Create the item, order and orderItem model

---
+++ Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISAD251.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public decimal Total { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public virtual List<OrderItem> OrderItem { get; set; }
+    }
+}
