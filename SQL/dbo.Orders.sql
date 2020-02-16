4fe31c80b8cae801db878f6462247c933edf0bcc
travislai0706 <58297822+travislai0706@users.noreply.github.com>
2020-02-17 02:24:25 +0800

    upload all appplication design documents and factsheet

---
+++ SQL/dbo.Orders.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [OrderId]     INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]      INT           NULL,
+    [CreatedDate] DATETIME2 (7) NULL,
+    PRIMARY KEY CLUSTERED ([OrderId] ASC)
+);
+
